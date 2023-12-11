7728ca8652a1eae345430c20eed7424cdeaa90e7
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-12-11 22:29:01 +0300

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,20 @@
+export type TListItem = {
+    id: number,
+    image: string,
+    name: string,
+    price: number,
+}
+
+export type TState = {
+    cart: {
+        itemsCount: number,
+        items: TListItem[],
+    },
+    catalog: {
+        list: TListItem[],
+        itemsCount: number
+    }
+}
+
+export type TCatalogPayload =  TState['catalog']['list']
+export type TCartPayload = number
