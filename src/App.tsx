d11f617057996a132eccfa2314d008ec4060c4b1
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-12-11 22:34:06 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Header } from './components/Header/Header';
+import { Catalog } from './components/Catalog/Catalog';
+import { Cart } from './components/Cart/Cart';
 
 export const App = () => {
 	return (
-		<div></div>
+		<BrowserRouter> 
+		<Header/>
+		<div className="content">
+			<Routes>
+				<Route path='/catalog' element={<Catalog/>}/>
+				<Route path='/cart' element={<Cart/>}/>
+			</Routes>
+		</div>
+	</BrowserRouter>
 	)
 }
 
