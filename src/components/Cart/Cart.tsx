18752e23606812e148ca118357856712cf746e4f
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-12-12 01:18:00 +0300

    create Cart component and add styles

---
+++ src/components/Cart/Cart.tsx
@@ -0,0 +1,68 @@
+import { useSelector, useDispatch } from "react-redux";
+import './Cart.css'
+import { TListItem, TState } from "../../types";
+import emptyCart from '../../images/emptyCart.svg'
+import { cart } from "../../redux/actionCreators";
+import { useMemo } from "react";
+
+export const Cart = () => {
+    const {items, itemsCount} = useSelector((state: TState) => state.cart)
+    const dispatch = useDispatch()
+
+    const sum = useMemo(()=> 
+        items.reduce((acc, el)=> el.price + acc, 0), 
+        [items]
+    )
+    
+    const deleteItem = (id: TListItem['id']) => dispatch(cart.delete(id))
+    return (
+        <div className="cart-container">
+            { 
+                itemsCount > 0 ?
+                    <div>
+                        <div className='result-sum'>
+                            <p>Итого:</p>
+                            <div>{sum} ₽</div>
+                        </div>
+                        <div className="cart-list">
+                            {
+                                items.map((el) => 
+                                    <div className="card">
+                                        <div className="card-content">
+                                            <img 
+                                                src={el.image} 
+                                                alt="cardImg" 
+                                                className="card-content__img"
+                                            />
+                                            <div className="card-list-description"> 
+                                                <p className="card-list-description__title">{el.name}</p>
+                                                <p>{el.price}₽</p>
+                                            </div>
+                                        </div>
+                                        
+                                        <div>
+                                            <button 
+                                                className="card-delete-btn"
+                                                onClick={() => deleteItem(el.id)}
+                                            >
+                                                Убрать
+                                            </button>
+                                        </div>
+                                    </div>
+                                ) 
+                            }
+                        </div>
+                    </div>
+                : 
+                <div className="empty-cart">
+                    <img 
+                        src={emptyCart} 
+                        alt="emptyCart"  
+                        className="empty-cart__img"
+                    />
+                    <p className="empty-cart__title">Корзина пуста</p>
+                </div>
+            } 
+        </div>
+    )  
+}
