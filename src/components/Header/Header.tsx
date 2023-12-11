3c4828108584d2cfca1a2c68b168c8ccfd7fdde7
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-12-11 20:05:55 +0300

    add Header component and styles

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,38 @@
+import { NavLink } from 'react-router-dom'
+import cart from '../../images/cart.svg'
+import { useSelector } from 'react-redux'
+import { TState } from '../../redux/store'
+import './Header.css'
+
+export const Header = () => {
+    const {itemsCount} = useSelector((state: TState) => state.cart)
+    return (
+        <header className='header header_sticky'>
+            <div className='nav-menu-and-logo-container'>
+                <h1 className='shop-logo'>Батарейка Shop</h1>
+
+                <div className='nav-menu'> 
+                    <NavLink 
+                        to='/catalog' 
+                        className='nav-menu__link'
+                    >
+                        Каталог
+                    </NavLink>
+                </div>
+            </div>
+            
+            <div className='cart-item'> 
+                <NavLink to='/cart' className='cart-item__link'>
+                    <img
+                        src={cart}
+                        alt="cart"
+                    />
+                    {
+                        itemsCount > 0 && 
+                        <p className='cart-item__link__items-count'>{itemsCount}</p>
+                    }
+                </NavLink>
+            </div>
+        </header>
+    )
+}
