bffbb38fce48cab645414746d1832927d16bc62d
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-12-12 13:23:08 +0300

    Update Home.tsx

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,15 +1,15 @@
-import home from '../../images/home.svg'
-import './Home.css'
+import home from "../../images/home.svg"
+import "./Home.css"
 
 export const Home = () => {
     return (
-        <div className='home-page'>
+        <div className="home-page">
             <img 
                 src={home} 
                 alt="groceryPackage"
-                className='home-page__img'
+                className="home-page__img"
             />
-            <p className='home-page__title'>
+            <p className="home-page__title">
                 Скоро мы добавим информацию о нашем магазине, 
                 а пока перейдите в каталог для совершения покупок
             </p>
