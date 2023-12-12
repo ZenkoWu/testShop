7bf266e8cd6f1925b4ac1294da748e64338dcb18
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-12-12 13:24:40 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,37 +1,37 @@
-import { NavLink } from 'react-router-dom'
-import cart from '../../images/cart.svg'
-import { useSelector } from 'react-redux'
-import { TState } from '../../types'
-import './Header.css'
+import { NavLink } from "react-router-dom"
+import cart from "../../images/cart.svg"
+import { useSelector } from "react-redux"
+import { TState } from "../../types"
+import "./Header.css"
 
 export const Header = () => {
     const {itemsCount} = useSelector((state: TState) => state.cart)
     return (
-        <header className='header header_sticky'>
-            <div className='nav-menu'> 
+        <header className="header header_sticky">
+            <div className="nav-menu"> 
                 <NavLink  
-                    to='/' 
-                    className='shop-logo'
+                    to="/" 
+                    className="shop-logo"
                 >
                     Батарейка Shop
                 </NavLink>
                 <NavLink 
-                    to='/catalog' 
-                    className='nav-menu__link'
+                    to="/catalog" 
+                    className="nav-menu__link"
                 >
                     Каталог
                 </NavLink>
             </div>
             
-            <div className='cart-item'> 
-                <NavLink to='/cart' className='cart-item__link'>
+            <div className="cart-item"> 
+                <NavLink to="/cart" className="cart-item__link">
                     <img
                         src={cart}
                         alt="cart"
                     />
                     {
                         itemsCount > 0 && 
-                        <p className='cart-item__link__items-count'>{itemsCount}</p>
+                        <p className="cart-item__link__items-count">{itemsCount}</p>
                     }
                 </NavLink>
             </div>
