64e7487deadf72ba172ebaacee99aa5627ff814d
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-12-12 13:26:04 +0300

    Update apiRoutes.ts

--- src/redux/apiRoutes.ts
+++ src/redux/apiRoutes.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = 'https://appevent.ru/dev/task1/';
+export const BASE_URL = "https://appevent.ru/dev/task1/";
 
 export const API_ROUTES = {
-   CATALOG: 'catalog'
+   CATALOG: "catalog"
 }
