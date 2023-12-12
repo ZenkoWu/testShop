bfe232703c8cb4354cd6f6bb6c7cc675d653e85a
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-12-12 13:26:36 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import {App} from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
-const root = createRoot(document.getElementById('root')  as Element);
+const root = createRoot(document.getElementById("root")  as Element);
 root.render(
     <StrictMode>
         <Provider store={store}>
