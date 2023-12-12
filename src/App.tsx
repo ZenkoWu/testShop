79e8fdca06fda629396e2a69caae2b5806218527
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-12-12 13:26:24 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import { Header } from './components/Header/Header';
-import { Catalog } from './components/Catalog/Catalog';
-import { Cart } from './components/Cart/Cart';
-import { Home } from './components/Home/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Header } from "./components/Header/Header";
+import { Catalog } from "./components/Catalog/Catalog";
+import { Cart } from "./components/Cart/Cart";
+import { Home } from "./components/Home/Home";
 
 export const App = () => {
 	return (
@@ -11,9 +11,9 @@ export const App = () => {
 		<Header/>
 		<div className="content">
 			<Routes>
-				<Route index path='/' element={<Home/>}/>
-				<Route index path='/catalog' element={<Catalog/>}/>
-				<Route path='/cart' element={<Cart/>}/>
+				<Route index path="/" element={<Home/>}/>
+				<Route index path="/catalog" element={<Catalog/>}/>
+				<Route path="/cart" element={<Cart/>}/>
 			</Routes>
 		</div>
 	</BrowserRouter>
